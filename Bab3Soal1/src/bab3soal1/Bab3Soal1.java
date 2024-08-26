dd62391392eb055675565fb128a7b5887f4362dd
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-26 17:34:13 +0700

    Add files via upload

---
+++ Bab3Soal1/src/bab3soal1/Bab3Soal1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab3Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Var1 = 9;
+        float Var2 = 8.99f;
+        char Var3 = 'A';
+        boolean Var4 = false;
+        String Var5 = "Hello World";
+        
+        System.out.println(Var1);
+        System.out.println(Var2);
+        System.out.println(Var3);
+        System.out.println(Var4);
+        System.out.println(Var5);
+       
+    }
+    
+}
