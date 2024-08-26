c88b5807084da931385d0c5d36947c063acc492a
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-26 17:52:41 +0700

    Add files via upload

---
+++ Bab3Soal2/src/bab3soal2/Bab3Soal2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab3Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NIM = 2110117000;
+        String mahasiswa = "Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        float usia = 19.5f;
+        double tinggi = 1.72;
+        
+        System.out.println("NIM             : " + NIM);
+        System.out.println("Nama Mahasiswa  : " + mahasiswa);
+        System.out.println("Kelas           : " + kelas);
+        System.out.println("Usia            : " + usia);
+        System.out.println("Tinggi          : " + tinggi);
+        
+    }
+    
+}
