c76e1845ea9962f1671584ca44b32edefe0d5cfe
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-26 18:42:50 +0700

    Add files via upload

---
+++ Bab4Soall1/src/bab4soall1/Bab4Soall1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soall1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab4Soall1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("Hasil Pertambahan: " + (angka1 + angka2 ) );
+        System.out.println("Hasil Pengurangan: " + (angka1 - angka2) );
+        System.out.println("Hasil Perkalian  : " + (angka1 * angka2) );
+        System.out.println("Hasil Pembagian  : " + (angka1 / angka2) );
+    }
+    
+}
