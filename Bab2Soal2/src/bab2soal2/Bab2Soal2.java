de8c8d79bbe3ddb0b00cb3213f6d2e2d2a6057c7
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-26 16:58:06 +0700

    Add files via upload

---
+++ Bab2Soal2/src/bab2soal2/Bab2Soal2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal2;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab2Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String makanan = "ayam geprek";
+        String minuman = "jus jambu";
+        
+        System.out.println("makanan favorit saya : " + makanan);
+        System.out.println("minuman favorit saya : " + minuman);
+    }
+    
+}
