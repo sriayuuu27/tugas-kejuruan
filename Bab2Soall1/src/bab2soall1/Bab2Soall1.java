6c5d362fb34012350e7e4e2116b422d93001f83b
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-26 16:57:16 +0700

    Add files via upload

---
+++ Bab2Soall1/src/bab2soall1/Bab2Soall1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soall1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab2Soall1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=25;
+        int b=25;
+        int c=a+b;
+        System.out.println(c); 
+    }
+    
+}
