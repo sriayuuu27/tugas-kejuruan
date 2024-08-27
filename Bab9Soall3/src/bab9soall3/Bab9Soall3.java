05fc4f59a430d80fa84be9be455ba14a09a2a8ee
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-27 11:47:58 +0700

    Add files via upload

---
+++ Bab9Soall3/src/bab9soall3/Bab9Soall3.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab9soall3;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab9Soall3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+        while (i < 6) {
+        System.out.println(i);
+        i++;
+        }
+    }
+    
+}
