abca48199a755b1474670b694d6f88909f394ae2
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-27 11:47:06 +0700

    Add files via upload

---
+++ Bab9Soal1/src/bab9soal1/Bab9Soal1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab9soal1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab9Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+        System.out.print("* ");
+        }
+        System.out.println();//baris baru
+        }
+    }
+    
+}
