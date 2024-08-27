150db07c3e344129426cd8dfdc967bbfa9d70642
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-27 12:48:14 +0700

    Add files via upload

---
+++ Bab10Soall1/src/bab10soall1/Aritmatika.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab10soall1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Aritmatika {
+    public int pengurangan(int a, int b) {
+        return a - b;
+    }
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+    public int pembagian(int a, int b) {
+    
+        if (b == 0) {
+            throw new ArithmeticException("Tidak bisa membagi dengan nol");
+        }
+        return a / b;
+    }
+    public int pangkat(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+}
+
---
+++ Bab10Soall1/src/bab10soall1/Bab10Soall1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab10soall1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab10Soall1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Aritmatika aritmatika = new Aritmatika();
+
+        System.out.println("Pengurangan: " + aritmatika.pengurangan(10, 5));
+        System.out.println("Perkalian: " + aritmatika.perkalian(10, 5));
+        System.out.println("Pembagian: " + aritmatika.pembagian(10, 2));
+        System.out.println("Pangkat: " + aritmatika.pangkat(2, 3));
+    }
+}
+
+    
