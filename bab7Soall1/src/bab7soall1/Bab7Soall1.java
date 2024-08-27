99d8e0ab02e5e610993222a02e6aab4bf09b9555
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-27 10:21:59 +0700

    Add files via upload

---
+++ bab7Soall1/src/bab7soall1/Bab7Soall1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab7soall1;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab7Soall1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+            default:
+                System.out.println("Hari tidak valid");
+                break;
+        }
+    }
+    
+}
