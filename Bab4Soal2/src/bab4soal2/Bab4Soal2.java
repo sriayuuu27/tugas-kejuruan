18324bbb2287706fab1016470551355f6c6f61c8
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-27 07:02:51 +0700

    Add files via upload

---
+++ Bab4Soal2/src/bab4soal2/Bab4Soal2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal2;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab4Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int w, x, y, z;
+        w = 5;
+        x = 6;
+        y = 8-x++;
+        z = 8-++w;
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+    
+}
