187b9021401617d7cebb0763b10bc26f16b64f59
sriayuuu27 <144987415+sriayuuu27@users.noreply.github.com>
2024-08-29 15:22:49 +0700

    Add files via upload

---
+++ Bab14Soal2/src/bab14soal2/Bab14Soal2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab14soal2;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Bab14Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Burung burung = new Burung();
+       
+       burung.suara();
+       
+       burung.terbang();
+    }
+}
---
+++ Bab14Soal2/src/bab14soal2/Burung.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bab14soal2;
+
+/**
+ *
+ * @author Sriay
+ */
+public class Burung {
+    void suara() {
+        System.out.println("Burung Berkicau");
+    }
+    void terbang() {
+        System.out.println("Burung Terbang di Udara");
+    }
+    
+    void makan() {
+        System.out.println("Hewan Ini Makan");
+    }
+}
